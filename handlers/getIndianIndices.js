975930b481e272b7b57666e3e5281f0a633be577
Akshit Aryan <aryanakshit152@gmail.com>
2024-01-03 20:33:56 +0530

    deployment fix v2

--- handlers/getIndianIndices.js
+++ handlers/getIndianIndices.js
@@ -18,7 +18,12 @@ const getIndianIndices = async (screenerLink) => {
   dotenv.config();
   const browser = await puppeteer.launch({
     headless: "new",
-    args: ["--single-process", "--no-zygote", "--no-sandbox"],
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
--- handlers/getStockData.js
+++ handlers/getStockData.js
@@ -53,7 +53,12 @@ const getStockData = async (request, response) => {
 const getStockChartAndTables = async (screenerLink) => {
   const browser = await puppeteer.launch({
     headless: "new",
-    args: ["--single-process", "--no-zygote", "--no-sandbox"],
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
